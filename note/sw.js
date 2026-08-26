3b8adf00846e9cbb5a48ec0c698f07b86968d448
yuki08173123-svg <yuki08173123@gmail.com>
2026-08-26 11:57:12 +0900

    ReNote v5：フォルダ機能を追加、メール配信を廃止、画面デザインを全面刷新

--- note/sw.js
+++ note/sw.js
@@ -2,7 +2,7 @@
    ほうしん:「ネットワーク優先」。まずネットから最新をとり、とれたものをキャッシュに控える。
    ネットがないときだけキャッシュで動く。キャッシュを先に見る方式にすると
    「直したのに古い画面が出る」事故になるので、ぜったいにしない。 */
-const VER = 'mn-v4';
+const VER = 'mn-v5';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil((async () => {
